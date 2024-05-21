f2c0a32429e39614d9ebe5bb11f451d10e33f267
funxxfun <23shirasawa@gmail.com>
2024-05-21 14:32:40 +0900

    first commit

---
+++ app/controllers/private_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+class PrivateController < ActionController::API
+  before_action :authorize
+
+  include Secured
+
+  def private
+    # スコープの検証に成功した場合の処理
+    render json: 'Hello from a private endpoint! You need to be authenticated to see this.'
+  end
+
+  def private_scoped
+    validate_permissions ['read:messages'] do
+      # スコープの検証に成功した場合の処理
+      render json: { message: 'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.' }
+    end
+  end
+end
